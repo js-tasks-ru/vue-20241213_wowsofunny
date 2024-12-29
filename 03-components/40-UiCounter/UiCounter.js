aa5e3ef5eb0b4b12c4c9c9610714f128f90b2701
Vorobev Pavel <pavel_9_5@mail.ru>
2024-12-29 19:49:37 +0100

    Задача 03-components_40-UiCounter

--- 03-components/40-UiCounter/UiCounter.js
+++ 03-components/40-UiCounter/UiCounter.js
@@ -9,15 +9,41 @@ export default defineComponent({
     UiButton,
   },
 
-  setup() {
+  props: {
+    count: {
+      type: Number,
+      default: 0,
+    },
+
+    min: {
+      type: Number,
+      default: 0,
+    },
+
+    max: {
+      type: Number,
+      default: Infinity,
+    }
+  },
+
+  emits: ['update:count'],
+
+  setup(props, { emit }) {
     // Рекомендуется для практики реализовать обработку событий внутри setup, а не непосредственно в шаблоне
+    const increaseCounter = () => emit('update:count', props.count + 1);
+    const decreaseCounter = () => emit('update:count', props.count - 1);
+
+    return {
+      increaseCounter,
+      decreaseCounter
+    }
   },
 
   template: `
     <div class="counter">
-      <UiButton aria-label="Decrement" disabled>➖</UiButton>
-      <span class="count" data-testid="count">3</span>
-      <UiButton aria-label="Increment">➕</UiButton>
+      <UiButton aria-label="Decrement" @click="decreaseCounter" :disabled="count === min">➖</UiButton>
+      <span class="count" data-testid="count">{{ count }}</span>
+      <UiButton aria-label="Increment" @click="increaseCounter" :disabled="count === max">➕</UiButton>
     </div>
   `,
 })
