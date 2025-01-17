7521472ca4fc7ce4aca9ceef0c7d09cee0d6c8d8
Vorobev Pavel <pavel_9_5@mail.ru>
2025-01-17 17:17:54 +0100

    Задача 05-ts_20-UiFormGroup

--- 05-ts/20-UiFormGroup/UiFormGroup.vue
+++ 05-ts/20-UiFormGroup/UiFormGroup.vue
@@ -1,13 +1,39 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import type { Slot } from 'vue'
+
+const props = defineProps<{
+  for?: string,
+  label?: string,
+  description?: string,
+  hint?: string
+  showHint?: boolean,
+  invalid?: boolean,
+}>()
+
+defineSlots<{
+  default: Slot
+  label: Slot,
+  description: Slot,
+}>()
+</script>
 
 <template>
   <div class="form-group">
     <div class="form-group__label-wrapper">
-      <label for="FOR" class="form-group__label">LABEL</label>
-      <div class="form-group__description">DESCRIPTION</div>
+      <label :for="props.for" class="form-group__label">
+        <slot name="label">{{ label }}</slot>
+      </label>
+      <div class="form-group__description">
+        <slot name="description">{{ description }}</slot>
+      </div>
     </div>
-    <div class="form-group__control">CONTENT</div>
-    <div class="form-group__hint form-group__hint--invalid">HINT | ERROR</div>
+    <div class="form-group__control">
+      <slot></slot>
+    </div>
+    <div v-if="hint !== undefined" class="form-group__hint" :class="{ 'form-group__hint--invalid': invalid }">
+      <span v-if="showHint">{{ hint }}</span>
+      <span v-if="invalid">{{ hint }}</span>
+    </div>
   </div>
 </template>
 
